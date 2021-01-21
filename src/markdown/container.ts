c59381aea4d4d02497754974c646f914c2562efc
Masato Suzuki <suzuki.m.htym@gmail.com>
2021-01-22 04:18:09 +0900

    [from now] 2021/01/22 04:18:09
    diff --git a/src/markdown/container.ts b/src/markdown/container.ts
    index 2f172f5..fc292c7 100644
    --- a/src/markdown/container.ts
    +++ b/src/markdown/container.ts
    @@ -5,11 +5,9 @@ module.exports = (md: MarkdownIt)  => md.use(require('markdown-it-container'), '
       validate: () => true,
       render: (tokens: Token[], idx: number) => {
         if (tokens[idx].nesting === 1) {
    -      let headerClass = "container-header ";
    -      const attrs = tokens[idx].attrs || [];
    -      headerClass += attrs.filter(el => el[0] === 'class').map(el => el[1]).join(' ');
    -      // return `<div class="container"><div class="${headerClass}">${tokens[idx].info.trim()}</div><div class="container-content">`;
    -      return `<div class="container"><div class="${headerClass}">${md.render(tokens[idx].info.trim())}</div><div class="container-content">`;
    +      const header = tokens[idx].info.split(" ").filter(t => !t.startsWith(".")).join(" ");
    +      const classes = tokens[idx].info.split(" ").filter(t => t.startsWith(".")).map(t => t.slice(1, )).join(" ");
    +      return `<div class="container ${classes}"><div class="container-header">${md.render(header)}</div><div class="container-content">`;
         } else {
           return '</div></div>\n';
         }
    diff --git a/styles/mdstyle.css b/styles/mdstyle.css
    index f8b2303..dcd64b0 100644
    --- a/styles/mdstyle.css
    +++ b/styles/mdstyle.css
    @@ -30,13 +30,26 @@
     .purple {
       color: #b48ead;
     }
    +.container {
    +  padding: 0.5em 1em;
    +  margin: 0.5em;
    +  border-radius: 0.5em;
    +}
    +.container.gray {
    +  background: #d3d3d3;
    +}
    +.container.lightgray {
    +  background: #ebebeb;
    +}
     .container .container-header {
       font-weight: 500;
     }
    +.container .container-header > p{
    +  line-height: 1.5em;
    +}
     .container .container-content {
       margin-left: 0.5em;
       padding-left: 0.5em;
    -  border-left: 4px solid;
     }
     .bib .authors,
     .bib .year {

--- src/markdown/container.ts
+++ src/markdown/container.ts
@@ -5,11 +5,9 @@ module.exports = (md: MarkdownIt)  => md.use(require('markdown-it-container'), '
   validate: () => true,
   render: (tokens: Token[], idx: number) => {
     if (tokens[idx].nesting === 1) {
-      let headerClass = "container-header ";
-      const attrs = tokens[idx].attrs || [];
-      headerClass += attrs.filter(el => el[0] === 'class').map(el => el[1]).join(' ');
-      // return `<div class="container"><div class="${headerClass}">${tokens[idx].info.trim()}</div><div class="container-content">`;
-      return `<div class="container"><div class="${headerClass}">${md.render(tokens[idx].info.trim())}</div><div class="container-content">`;
+      const header = tokens[idx].info.split(" ").filter(t => !t.startsWith(".")).join(" ");
+      const classes = tokens[idx].info.split(" ").filter(t => t.startsWith(".")).map(t => t.slice(1, )).join(" ");
+      return `<div class="container ${classes}"><div class="container-header">${md.render(header)}</div><div class="container-content">`;
     } else {
       return '</div></div>\n';
     }
